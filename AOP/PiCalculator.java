115f06176da27f0110cacc39113a02f50cce8e26
DESKTOP-URKE2VL\SSAFY <ihyeonseo@naver.com>
2026-01-18 23:05:27 +0900

    AOP - 진행 중

---
+++ AOP/PiCalculator.java
@@ -0,0 +1,23 @@
+public class PiCalculator {
+    public static void main(String[] args) {
+        PiCalculator pi = new PiCalculator();
+        System.out.println(pi.calculate(10000000));
+    }
+
+    @PrintExcutionTime
+    double calculate(int points) {
+//        long start = System.currentTimeMillis();
+        int circle = 0;
+        for (long i = 0; i < points; i++){
+            double x = Math.random() * 2 - 1;
+            double y = Math.random() * 2 - 1;
+            if (x * x + y * y <= 1) {
+                circle++;
+            }
+        }
+
+//        long executionTime = System.currentTimeMillis() - start;
+//        System.out.println("executed in " + executionTime + "ms.");
+        return 4.0 * circle / points;
+    }
+}
